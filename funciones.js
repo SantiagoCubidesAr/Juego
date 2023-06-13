04707fc6ada2cb9a4f1f91d813789e7ccfbec0f6
SantiagoCubidesAr <santiagocubidesar@gmail.com>
2023-06-13 16:35:37 -0500

    Se empieza a realizar el codigo javascript para que empiece la pelea

--- funciones.js
+++ funciones.js
@@ -1,7 +1,11 @@
-const inicio = document.getElementById('ataque')
-
-function iniciar(evento) {
-    
+const ataque = document.getElementById('ataque')
+const defensa = document.getElementById('defensa')
+let vidaKaido = 100;
+let vidaLuffy = 100;
+const danoTabla = [[10,20,0],[0,10,20],[20,0,10]];
+function iniciar(ataque,defensa) {
+    const dano = danoTabla[ataque][defensa];
+    return dano;
 }
 
 inicio.addEventListener('click',iniciar)
