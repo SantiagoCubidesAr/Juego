bce6ed52091c2365e2bff6368f007c7b9699da94
SantiagoCubidesAr <santiagocubidesar@gmail.com>
2023-06-19 15:34:42 -0500

    Se trae todo de github

--- funciones.js
+++ funciones.js
@@ -1,9 +1,9 @@
-const ataque = document.getElementById('ataque')
-const defensa = document.getElementById('defensa')
+const ataques_kaido = document.getElementsByClassName('ataques-kaido')
+const ataques_luffy = document.getElementsByClassName('ataques-luffy')
 let vidaKaido = 100;
 let vidaLuffy = 100;
 const danoTabla = [[10,20,0],[0,10,20],[20,0,10]];
-function iniciar(ataque,defensa) {
+function iniciar(ataque) {
     const dano = danoTabla[ataque][defensa];
     return dano;
 }
