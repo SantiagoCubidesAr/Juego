863e99c9268a70ee4b1999598a979b6ba60dcf81
SantiagoCubidesAr <santiagocubidesar@gmail.com>
2023-06-24 10:31:04 -0500

    Se modifican varias cosas

--- funciones.js
+++ funciones.js
@@ -1,35 +1,7 @@
-<<<<<<< HEAD
-const ataques_kaido = document.getElementsByClassName('ataques-kaido')
-const ataques_luffy = document.getElementsByClassName('ataques-luffy')
-let vidaKaido = 100;
-let vidaLuffy = 100;
-const danoTabla = [[10,20,0],[0,10,20],[20,0,10]];
-function iniciar(ataque) {
-    const dano = danoTabla[ataque][defensa];
-    return dano;
-}
+let ataque = document.getElementsByClassName('ataques-luffy');
+console.log(ataque);
+ataque.click == "puño" ? ataque = "puño"
+: ataque.click == "patada" ? ataque = "patada"
+: ataque = "cabezazo";
+console.log(ataque);
 
-inicio.addEventListener('click',iniciar)
-=======
-const ataque = document.getElementById('boton-ataque')
-const puño = document.getElementById('puño')
-const patada = document.getElementById('patada')
-const cabezazo = document.getElementById('cabezazo')
-let ataqueJugador;
-let vidaKaido = 100;
-let vidaLuffy = 100;
-const danoTabla = [[10,20,0],[0,10,20],[20,0,10]];
-ataqueJugador === (puño.click) ? "puño" 
-    : (patada.click) ? "patada"
-    : "cabezazo"
-    console.log(ataqueJugador)
- ataque.addEventListener('click',seleccionarataque)
-
-
-function seleccionarataque(e) {
-    e.prevenDefault()
-
-    
-}
-
->>>>>>> 890a1bf3e9e61ac081c3809a5dadc603e82f452d
